a599956c898dd76999984ece7770db9cd83a2d0e
Tanel Vari <tavari@microsoft.com>
2016-10-27 15:47:30 +0300

    Väike JavaFX programmike ainekursuse 2. vaheetapiks. Joonistab ekraanile mõned nelinurgad,

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # HelloItCollege
-my very first GitHub project for IT College
+My very first GitHub project for IT College
 
-Started by creating Java project on computer and then creating a repo for it in GitHub...
+IT Kolledži õppeaine I200 "Programmeerimise algkursus Java baasil"
+
+Programm 2. vaheetapiks.
+
+Pisike JavaFX programm joonistab aknasse erinevate suuruste ja omadustega nelinurgad. 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,51 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class HelloWorld {
+import java.util.Random;
+
+public class HelloWorld extends Application {
+
+	private static final int SCENE_WIDTH = 640;
+	private static final int SCENE_HEIGHT = 480;
+
+	private static final int MIN_AMOUNT = 20;
+
+	@Override
+	public void start(Stage primaryStage){
+
+		Group root = new Group();
+		Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, Color.rgb(255, 255, 255));
+
+		Random random = new Random(System.currentTimeMillis());
+
+		for (int i = 0; i < (random.nextInt(20) + MIN_AMOUNT); i++){
+			Rectangle rectangle = new Rectangle();
+
+			int width = random.nextInt(SCENE_WIDTH / 2) + 10;
+			int height = random.nextInt(SCENE_HEIGHT / 2) + 10;
+			double opacity = random.nextInt(95) * 0.01;
+
+			rectangle.setWidth(width);
+			rectangle.setHeight(height);
+			rectangle.setX(random.nextInt(SCENE_WIDTH - width));
+			rectangle.setY(random.nextInt(SCENE_HEIGHT - height));
+			rectangle.setRotate(random.nextInt(90));
+			rectangle.setFill(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255), opacity));
+
+			root.getChildren().add(rectangle);
+		}
+
+		primaryStage.setTitle("Interlude 2 - Graphics in JavaFX");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Tere-tere vana kere");
+		launch(args);
 	}
 
 }
